4d5101340215335e3a1b71525384e52b104d49e2
Sercan Sebetçi <86542016+sebetci@users.noreply.github.com>
2022-02-18 21:12:51 +0300

    add: source code

---
+++ source/main.hpp
@@ -0,0 +1,35 @@
+/****************************************************************************
+ * Copyright (C) 2021 by Sercan Sebetçi                                     *
+ *                                                                          *
+ * This file is part of C-Fundamentals.                                     *
+ *                                                                          *
+ *   Box is free software: you can redistribute it and/or modify it         *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   Box is distributed in the hope that it will be useful,                 *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with Box.  If not, see <http://www.gnu.org/licenses/>.   *
+ ****************************************************************************/
+
+/**
+ * @file    main.hpp
+ * @author  Sercan Sebetçi
+ * @date    04.01.2022
+ * @brief   Pure Virtual Functions and Abstract Classes
+ * @see     https://github.com/sebetci/CPP-Fundamentals
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+#endif /* MAIN_H_ */
